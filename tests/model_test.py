285b15399189e119c8d18bbb58878d2784a060cd
Biprayan Choudhuri <biprayanchoudhuri58@gmail.com>
2025-10-03 22:26:12 +0530

    Add model loading tests and update CI workflow to include testing step

---
+++ tests/model_test.py
@@ -0,0 +1,41 @@
+import unittest
+import mlflow
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class TestModelLoading(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # Set up DagsHub credentials for MLflow tracking
+        dagshub_token = os.getenv("DAGSHUB_PATH")
+        if not dagshub_token:
+            raise EnvironmentError("DAGSHUB_PATH environment variable is not set")
+
+        os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+        os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+
+        # Set up MLflow tracking URI
+        mlflow.set_tracking_uri('https://dagshub.com/CodeNeuron58/Text-Classification--MLOps.mlflow')
+
+        # Load the model from MLflow model registry
+        cls.model_name = "LogR model"
+        cls.model_version = cls.get_latest_model_version(cls.model_name)
+        cls.model_uri = f'models:/{cls.model_name}/{cls.model_version}'
+        cls.model = mlflow.pyfunc.load_model(cls.model_uri)
+
+    @staticmethod
+    def get_latest_model_version(model_name):
+        client = mlflow.MlflowClient()
+        latest_version = client.get_latest_versions(model_name, stages=["Staging"])
+        return latest_version[0].version if latest_version else None
+
+    def test_model_loaded_properly(self):
+        self.assertIsNotNone(self.model)
+
+if __name__ == "__main__":
+    unittest.main()
